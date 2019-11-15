10a0dedd978af2b35e4c7ffe723e492f9ab03f60
Nam Jeonghyun <ska827@snu.ac.kr>
2019-11-15 14:06:43 +0900

    Change provider field type from String to enum

--- src/provider.rs
+++ src/provider.rs
@@ -1,5 +1,32 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+use diesel::backend::Backend;
+use diesel::deserialize::{self, FromSql};
+use diesel::serialize::{self, Output, ToSql};
+use diesel::sql_types::Text;
+use diesel::sqlite::Sqlite;
+use std::io::Write;
+#[derive(AsExpression, FromSqlRow, Debug, Clone, Copy, PartialEq, Eq)]
+#[sql_type = "Text"]
 pub enum Provider {
     Lezhin,
     Naver,
+}
+
+impl ToSql<Text, Sqlite> for Provider {
+	fn to_sql<W: Write>(&self, out: &mut Output<W, Sqlite>) -> serialize::Result {
+		let value = match self {
+			Self::Lezhin => "lezhin",
+			Self::Naver => "naver",
+		};
+		<String as ToSql<Text, Sqlite>>::to_sql(&value.to_string(), out)
+	}
+}
+
+impl FromSql<Text, Sqlite> for Provider {
+	fn from_sql(bytes: Option<&<Sqlite as Backend>::RawValue>) -> deserialize::Result<Self> {
+		match unsafe{<*const str as FromSql<Text, Sqlite>>::from_sql(bytes)?.as_ref()}.unwrap() {
+			"lezhin" => Ok(Self::Lezhin),
+            "naver" => Ok(Self::Naver),
+			_ => Err("Unrecognized enum variant".into()),
+		}
+	}
 }
